6ed37024210f4aecff997d688884855b7ff1160c
Ramazan Ramazanov <ramazan.ramazanov@glion.ch>
2020-07-23 21:51:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,8 +3,8 @@ def using_concat ( my_favorite_things, more_favs )
 
 end
 
-def using_insert (list_of_programming_languages, another_language)
-  list_of_programming_languages.insert(4, 'Python')
+def using_insert (array, element)
+  list_of_programming_languages.insert(4, )
 end
 
 def using_uniq(haircuts)
